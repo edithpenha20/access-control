dcb6f114395e4098f293c6e39beaa387b8f7cc35
Edith Penha <edithpenha@gmail.com>
2021-06-26 20:45:51 -0400

    Created project and configuration of dependencies

---
+++ src/main/java/com/ponto/acesso/model/WorkingDay.java
@@ -0,0 +1,14 @@
+package com.ponto.acesso.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class WorkingDay {
+
+    private long id;
+    private String description;
+}
